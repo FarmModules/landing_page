6191c8555ccefe6733c3a2fe5603e2d9b06dc6eb
aron-helu <aronmilahela@gmail.com>
2023-06-01 13:55:57 +0300

    Add dev branch and customize the navigation bar.
    This commit adds a dev branch and customizes the navigation bar. The changes include:
    
    * Adding a new link to the navigation bar for the dev branch.
    * Changing the color of the navigation bar to blue.
    * Centering the links in the navigation bar.

--- src/app/Coomponent/Navigation.js
+++ src/app/Coomponent/Navigation.js
@@ -7,42 +7,28 @@ const Navigation = () => {
         <Link href="/" class="py-2 flex items-center text-xl font-bold text-gray-800">
           <img src="https://www.farmmodules.tech/wp-content/uploads/2021/09/cropped-FMlogo.png" alt="" class="h-10 mr-2"  />
         </Link>
-        <ul class="flex flex-col md:flex-row justify-content-between mx-auto justify-content-center py-8 ">
+        <ul class="flex flex-col md:flex-row justify-content-between mx-auto justify-content-center py-8 uppercase">
           <li class="nav-item">
-            <Link href="/" class="nav-link py-2 px-4 text-gray-600 text-align:center">
-              Home
+            <Link href="#Solutions" class="nav-link py-2 px-4 text-gray-600 text-align:center">
+             Solutions
             </Link>
           </li>
           <li class="nav-item">
-            <Link href="/about" class="nav-link py-2 px-4 text-gray-600 text-align:center">
+            <Link href="#About" class="nav-link py-2 px-4 text-gray-600 text-align:center">
               About
             </Link>
           </li>
           <li class="nav-item">
-            <Link href="/service" class="nav-link py-2 px-4 text-gray-600 text-align:center">
-              Service
+            <Link href="#Team" class="nav-link py-2 px-4 text-gray-600 text-align:center">
+              Team
             </Link>
           </li>
           <li class="nav-item">
-            <Link href="/portfolio" class="nav-link py-2 px-4 text-gray-600 text-align:center">
-              Portfolio
-            </Link>
-          </li>
-          <li class="nav-item">
-            <Link href="/testmonial" class="nav-link py-2 px-4 text-gray-600 text-align:center">
-              Testmonial
-            </Link>
-          </li>
-          <li class="nav-item">
-            <Link href="/blog" class="nav-link py-2 px-4 text-gray-600 text-align:center">
-              Blog
-            </Link>
-          </li>
-          <li class="nav-item">
-            <Link href="/contact" class="nav-link py-2 px-4 text-gray-600 text-align:center">
+            <Link href="#Contact" class="nav-link py-2 px-4 text-gray-600 text-align:center">
               Contact
             </Link>
           </li>
+          
         </ul>
       </div>
     </nav>
