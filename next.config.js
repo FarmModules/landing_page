eb187a924991ee9a4fc5b9631fc59a4ddf12eeeb
aron-helu <aronmilahela@gmail.com>
2023-07-10 17:44:02 +0300

    Fixtheimage path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+  reactStrictMode: true,
+
+  // Add basePath
+  basePath: '/github-pages',
+
+  // Enable static HTML export
+  output: 'export',
   }
 // next.config.js
 module.exports = {
